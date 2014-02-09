2d87f061063cacb085839674f3743ad0d4fd7370
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-02-09 19:28:36 +0100

    Modif du script de build

--- build_all.sh
+++ build_all.sh
@@ -9,3 +9,4 @@ cd MdC && $LATEX mdc.tex && $LATEX mdc.tex && cp mdc.pdf ../$BUILD ; cd ..
 cd MdE && $LATEX mde.tex && $LATEX mde.tex && cp mde.pdf ../$BUILD ; cd ..
 cd MdO && $LATEX mdo.tex && $LATEX mdo.tex && cp mdo.pdf ../$BUILD ; cd ..
 cd MdP && $LATEX mdp.tex && $LATEX mdp.tex && cp mdp.pdf ../$BUILD ; cd ..
+cd Qualite && $LATEX qualite.tex && $LATEX qualite.tex && cp qualite.pdf ../$BUILD ; cd ..
